a068d6b9ba3d076490f579112ad00c2d53110bc1
easysajusaju-dev <easysaju.saju@gmail.com>
2025-11-04 15:23:21 +0900

    Update products.js

--- aPI/products.js
+++ aPI/products.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const url = process.env.PRODUCTS_URL;
+    const url = process.env.https://script.google.com/macros/s/AKfycbwX6UPs_IaiyaHGMBdRrwUzoaAoe5EjM0JifNgw4K7DNPDX84QPfvwh16YAs0KhaRfx-g/exec;
     if (!url) throw new Error('Missing PRODUCTS_URL');
     const r = await fetch(url, { method: 'GET', cache: 'no-store' });
     if (!r.ok) throw new Error(`Apps Script error: ${r.status}`);
